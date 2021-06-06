4d2fd02278417089a5cd70adcadeec6377c294b6
George Jones <george@circleaudiovisual.com>
2021-06-07 00:29:24 +0100

    properties now display on page

--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,41 @@
-import { properties, utilities, stations } from './cells.js'
+import { cellOrder, properties } from './cells.js'
 
-function init(){
+function init() {
   const board = document.querySelector('.board')
-  const cells = [
-    'go',
-    'oldKentRoad',
-    'comunityChest',
-    'whitechapelRoad',
-    'incomeTax',
-    'kingsCrossStation',
-    'angelIslington',
-    'chance',
-    'eustonRoad',
-    'jail',
-    'pallMall',
-    'electric',
-    'whitehall',
-    'northumberlandAvenue',
-    'marylebone',
-    'bowStreet',
-    'comunityChest',
-    'marlboroughStreet',
-    'vineStreet',
-    'parking',
-    'strand',
-    'chance',
-    'fleetStreet',
-    'trafalgarSquare',
-    'fenchurchStreet',
-    'leicesterSquare',
-    'coventryStreet',
-    'water',
-    'piccadilly',
-    'goToJail',
-    'regentStreet',
-    'oxfordStreet',
-    'comunityChest',
-    'bondStreet',
-    'liverpoolStreet',
-    'chance',
-    'parkLane',
-    'superTax',
-    'mayfair'
-  ]
-  const notProperties = ['superTax', 'marylebone', 'comunityChest', 'liverpoolStreet', 'goToJail', 'water', 'fenchurchStreet', 'parking', 'comunityChest', 'go', 'comunityChest', 'incomeTax', 'kingsCrossStation', 'chance', 'jail', 'electric']
+  const cells = []
 
   function createBoard() {
     console.log(properties)
-    cells.forEach(cell => {
-      if (notProperties.includes(cell)) return
-
-      console.log(properties[cell])
+    cellOrder.forEach(cell => {
+      if (cell.type === 'property') {
+        const propertyCell = createPropertyCell(properties[cell.name])
+        cells.push(propertyCell)
+        board.appendChild(propertyCell)
+      } else if (cell.type === 'station'){
+        const stationCell = 
+      }
     })
+    console.log(cells)
   }
+
+  function createPropertyCell(info) {
+    const cell = document.createElement('div')
+    cell.className = `cell property ${info.name.replaceAll(' ', '-')}`
+    cell.innerHTML = `
+      <div class="header ${info.color.replaceAll(' ', '-')}" style="background-color:${info.color.replace(' ', '')}"></div>
+      <div class="title">
+        <h3>${info.name}</h3>
+      </div>
+      <div class="price">
+        <h4>£${info.cost}</h4>
+      </div>
+    `
+    return cell
+  }
+
+
+
+
 
   createBoard()
 }
